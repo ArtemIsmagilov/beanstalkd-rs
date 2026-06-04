720ff7bd197e0b78768864f5b971bca51ae3c713
Artem Ismagilov <artem.ismagilov.2000@mail.ru>
2026-06-04 11:31:31 +0400

    fix doc

--- src/lib.rs
+++ src/lib.rs
@@ -11,25 +11,41 @@
 //! ### Producer
 //!
 //! ```rust,no_run
-//! let mut conn = Connection::default().await?;
-//! let result = conn.put(0, 0, 60, b"Hello, Beanstalkd!").await?;
-//! match result {
-//!     PutResult::Inserted(id) => println!("Job inserted with ID: {}", id),
-//!     PutResult::Buried(id) => println!("Job buried with ID: {}", id),
+//! use beanstalkd_rs::{Connection, PutResult};
+//! use smol::block_on;
+//!
+//! fn main() -> beanstalkd_rs::Result<()> {
+//!     block_on(async {
+//!         let mut conn = Connection::default().await?;
+//!         let result = conn.put(0, 0, 60, b"Hello, Beanstalkd!").await?;
+//!         match result {
+//!             PutResult::Inserted(id) => println!("Job inserted with ID: {}", id),
+//!             PutResult::Buried(id) => println!("Job buried with ID: {}", id),
+//!         }
+//!         Ok(())
+//!     })
 //! }
 //! ```
 //!
 //! ### Consumer
 //!
 //! ```rust,no_run
-//! let mut conn = Connection::default().await?;
-//! match conn.reserve().await? {
-//!     ReserveResult::Reserved(job) => {
-//!         println!("Got job {}: {:?}", job.id, job.body);
-//!         conn.delete(job.id).await?;
-//!     }
-//!     ReserveResult::TimedOut => println!("No jobs available"),
-//!     ReserveResult::DeadlineSoon => println!("Worker deadline approaching"),
+//! use beanstalkd_rs::{Connection, ReserveResult};
+//! use smol::block_on;
+//!
+//! fn main() -> beanstalkd_rs::Result<()> {
+//!     block_on(async {
+//!         let mut conn = Connection::default().await?;
+//!         match conn.reserve().await? {
+//!             ReserveResult::Reserved(job) => {
+//!                 println!("Got job {}: {:?}", job.id, job.body);
+//!                 conn.delete(job.id).await?;
+//!             }
+//!             ReserveResult::TimedOut => println!("No jobs available"),
+//!             ReserveResult::DeadlineSoon => println!("Worker deadline approaching"),
+//!         }
+//!         Ok(())
+//!     })
 //! }
 //! ```
 
